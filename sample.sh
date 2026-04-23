e80877a340dbfc810ec955c7212b55f9b16ccbdf
Eren Cetin <erencetin98@gmail.com>
2026-04-23 23:49:43 +0200

    updated sampling script for Wan2.1 original repository

--- sample.sh
+++ sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROMPT="Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside."
-CKPT_DIR="/iopsstor/scratch/cscs/ecetin/MemoryKrea/.cache/huggingface/hub/models--Wan-AI--Wan2.1-I2V-14B-720P/snapshots/8823af45fcc58a8aa999a54b04be9abc7d2aac98"
+CKPT_DIR="/iopsstor/scratch/cscs/ecetin/MemoryKrea/.cache/huggingface/hub/models--Wan-AI--Wan2.1-T2V-1.3B/snapshots/37ec512624d61f7aa208f7ea8140a131f93afc9a"
 
 python generate.py \
     --ckpt_dir $CKPT_DIR \
